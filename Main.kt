55aa1b8bd85db7dae442aa775e8147a36fae4e2b
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-02-20 03:29:53 +0300

    Update Main.kt
    - set LSB in every color of every pixel to 1.

--- Main.kt
+++ Main.kt
@@ -1,5 +1,11 @@
 package cryptography
 
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
+
 fun main() {
     do {
         println("Task (hide, show, exit):")
@@ -32,13 +38,24 @@ fun handleHide() {
     val outputFileName = readLine()!!
     try {
         println("Input Image: $inputFileName")
-    } catch (_: Exception) {
-        println("Can't read input file!")
-    }
-
-    try {
+        val inputFile = File(inputFileName)
+        val image: BufferedImage = ImageIO.read(inputFile)
+        for (x in 0 until image.width) {
+            for (y in 0 until image.height) {
+                val color = Color(image.getRGB(x, y))
+                val green = color.green or 1
+                val blue = color.blue or 1
+                val red = color.red or 1
+                val newColor = Color(red, green, blue)
+                image.setRGB(x, y, newColor.rgb)
+            }
+        }
+        val outputFile = File(outputFileName)
+        ImageIO.write(image, "png", outputFile)
         println("Output Image: $outputFileName")
         println("Image $outputFileName is saved.")
+    } catch (_: IOException) {
+        println("Can't read input file!")
     } catch (_: Exception) {
         println("Can't read output file!")
     }
