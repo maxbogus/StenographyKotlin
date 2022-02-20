4d895c364ac70be058987051970643f4dc69a3a0
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-02-20 03:12:16 +0300

    Update Main.kt
    - implemented simple logic;
    - added error handling.

--- Main.kt
+++ Main.kt
@@ -1,5 +1,45 @@
 package cryptography
 
 fun main() {
+    do {
+        println("Task (hide, show, exit):")
+        val input = readLine()!!
+        when (input) {
+            "hide" -> {
+                handleHide()
+            }
+            "show" -> {
 
+            }
+            "exit" -> {
+                handleExit()
+            }
+            else -> {
+
+            }
+        }
+    } while (input != "exit")
+}
+
+private fun handleExit() {
+    println("Bye!")
+}
+
+fun handleHide() {
+    println("Input image file:")
+    val inputFileName = readLine()!!
+    println("Output image file:")
+    val outputFileName = readLine()!!
+    try {
+        println("Input Image: $inputFileName")
+    } catch (_: Exception) {
+        println("Can't read input file!")
+    }
+
+    try {
+        println("Output Image: $outputFileName")
+        println("Image $outputFileName is saved.")
+    } catch (_: Exception) {
+        println("Can't read output file!")
+    }
 }
